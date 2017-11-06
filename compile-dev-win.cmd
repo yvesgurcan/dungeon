c31418b57482c92f85276002f3b0c621e6b4eec8
Yves <gurcan.yves@gmail.com>
2017-11-06 09:28:50 -0800

    windows dev build cmd

---
+++ compile-dev-win.cmd
@@ -0,0 +1,14 @@
+@echo off
+echo.
+echo REACT_APP_RELEASE=dev > .env
+echo REACT_APP_VERSION=0.6.2 >> .env
+echo REACT_APP_STAGE=pre-alpha >> .env
+set currenttime=%time%
+if "%currenttime:~0,1%"==" " (SET currenttime=0%currenttime:~1%)
+echo REACT_APP_BUILD_TIME=%date:~10,4%/%date:~4,2%/%date:~7,2% %currenttime:~0,5% >> .env
+type .env
+npm run build
+@echo off
+move ./build ./dev
+echo build folder moved to /dev
+echo on
