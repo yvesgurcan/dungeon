ffffce5d9509278defc1323138f6384d4451b3e8
Yves <gurcan.yves@gmail.com>
2017-11-06 18:15:53 -0800

    windows compilation scripts

--- compile-dev-win.cmd
+++ compile-dev-win.cmd
@@ -7,8 +7,7 @@ set currenttime=%time%
 if "%currenttime:~0,1%"==" " (SET currenttime=0%currenttime:~1%)
 echo REACT_APP_BUILD_TIME=%date:~10,4%/%date:~4,2%/%date:~7,2% %currenttime:~0,5% >> .env
 type .env
+echo Do not forget to move /build to /dev.
 npm run build
-@echo off
-move ./build ./dev
-echo build folder moved to /dev
-echo on
+::move ./build ./dev
+::echo build folder moved to /dev
---
+++ compile-stable-win.cmd
@@ -0,0 +1,16 @@
+@echo off
+echo.
+echo REACT_APP_RELEASE=stable > .env
+echo REACT_APP_VERSION=0.6.2 >> .env
+echo REACT_APP_STAGE=pre-alpha >> .env
+
+
+
+
+
+
+type .env
+echo Do not forget to move /build to /stable.
+npm run build
+::move ./build ./stable
+::echo build folder moved to /stable
