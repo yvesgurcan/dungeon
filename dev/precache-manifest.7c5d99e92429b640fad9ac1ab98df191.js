4c1ac17ceaf2f0afca1efceee2e71f19de316db2
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-23 22:40:35 -0700

    New build

---
+++ dev/precache-manifest.7c5d99e92429b640fad9ac1ab98df191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8963c7b18c25ec35038f8a69a9fbcb42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cdf8f237cb1b0f9f205d",
+    "url": "./static/js/2.cf0477ba.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "./static/js/2.cf0477ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908e92a724022700e4fd",
+    "url": "./static/js/main.7fa54404.chunk.js"
+  },
+  {
+    "revision": "9ff77c26f65f0acfa1be",
+    "url": "./static/js/runtime-main.ba3d2c53.js"
+  }
+]);
---
+++ stable/precache-manifest.5d4a2c50d32012f7a02a6679f23d65be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2025b2f8cc71fecbd0d76d57ea194b7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "083ba28d9f5818591547",
+    "url": "./static/js/2.0d22b7c5.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "./static/js/2.0d22b7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17619301631315d2c2ad",
+    "url": "./static/js/main.3237ef50.chunk.js"
+  },
+  {
+    "revision": "9ff77c26f65f0acfa1be",
+    "url": "./static/js/runtime-main.ba3d2c53.js"
+  }
+]);
